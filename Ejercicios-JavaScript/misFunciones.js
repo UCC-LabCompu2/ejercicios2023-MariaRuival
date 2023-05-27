7c685c4e5143339a3a1cde19104cf6107bc2fc72
MariaRuival <2207446@ucc.edu.ar>
2023-05-27 16:46:18 -0300

    Se agregó el ejercico de Local Storage

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -6,104 +6,121 @@
  * @return
  */
 
-function cambiarUnidades(id, valor){
+function cambiarUnidades(id, valor) {
     var metro, pulgada, pie, yarda;
-    if(valor.includes(";")){
+    if (valor.includes(";")) {
         valor = valor.replace(",", ".");
     }
-    if(isNaN(valor)){
+    if (isNaN(valor)) {
         alert("Se ingreso un valor invalido" + id);
         metro = "";
         pulgada = "";
         pie = "";
         yarda = "";
-    }else if(id=="metro"){
+    } else if (id == "metro") {
         metro = valor;
-        pulgada = 39.3701*valor;
-        pie = 3.28084*valor;
-        yarda = 1.09361*valor;
-    }else if(id=="pulgada"){
+        pulgada = 39.3701 * valor;
+        pie = 3.28084 * valor;
+        yarda = 1.09361 * valor;
+    } else if (id == "pulgada") {
         pulgada = valor;
-        metro = 0.0254*valor;
-        pie = 0.0833333*valor;
-        yarda = 0.0277778*valor;
-    }else if(id=="pie"){
+        metro = 0.0254 * valor;
+        pie = 0.0833333 * valor;
+        yarda = 0.0277778 * valor;
+    } else if (id == "pie") {
         pie = valor;
         pulgada = 12 * valor;
-        metro = 0.3048*valor;
-        yarda = 0.333333*valor;
-    }else if(id=="yarda"){
+        metro = 0.3048 * valor;
+        yarda = 0.333333 * valor;
+    } else if (id == "yarda") {
         yarda = valor;
-        pulgada = 36*valor;
-        pie = 3*valor;
-        metro = 0.9144*valor;
+        pulgada = 36 * valor;
+        pie = 3 * valor;
+        metro = 0.9144 * valor;
     }
-    document.lasUnidades.unid_metro.value = Math.round(metro*100)/100;
-    document.lasUnidades.unid_pulgada.value = Math.round(pulgada*100)/100;
-    document.lasUnidades.unid_pie.value = Math.round(pie*100)/100;
-    document.lasUnidades.unid_yarda.value = Math.round(yarda*100)/100;
+    document.lasUnidades.unid_metro.value = Math.round(metro * 100) / 100;
+    document.lasUnidades.unid_pulgada.value = Math.round(pulgada * 100) / 100;
+    document.lasUnidades.unid_pie.value = Math.round(pie * 100) / 100;
+    document.lasUnidades.unid_yarda.value = Math.round(yarda * 100) / 100;
 }
 
-function convertirGR(id){
+function convertirGR(id) {
     var rad, grad;
-    if(id=="grados"){
+    if (id == "grados") {
         grad = document.getElementById(elementId:"grados").value;
-        rad = (grad*Math.PI)/180;
-    }else if(id=="radianes"){
-        rad = document.getElementById(elementId: "radianes").value;
-        grad = (rad*180)/Math.PI;
+        rad = (grad * Math.PI) / 180;
+    } else if (id == "radianes") {
+        rad = document.getElementById(elementId:"radianes").value;
+        grad = (rad * 180) / Math.PI;
     }
     document.getElementById(elementId:"grados").value = grad;
     document.getElementById(elementId:"radianes").value = rad;
 }
 
-function mostrar_ocultar(valorMO){
-    if(valorMO=="val_mostrar"){
+function mostrar_ocultar(valorMO) {
+    if (valorMO == "val_mostrar") {
         document.getElementById(elementId:"divMO").style.display = 'block';
-    }else if(valorMO=="valo_ocultar"){
+    } else if (valorMO == "valo_ocultar") {
         document.getElementById(elementId:"divMO").style.display = 'none';
     }
 }
 
-function calcularSuma(){
+function calcularSuma() {
     var num1, num2;
-    num1 = document.getElementsByName(elementName: "sum_num1")[0].value;
-    num2 = document.getElementsByName(elementName: "sum_num2")[0].value;
+    num1 = document.getElementsByName(elementName:"sum_num1")[0].value;
+    num2 = document.getElementsByName(elementName:"sum_num2")[0].value;
     document.getElementsByName(elementName:"sum_total")[0].innerHTML = Number(num1) + Number(num2);
 }
-function calcularResta(){
+
+function calcularResta() {
     var num1, num2;
-    num1 = document.getElementsByName(elementName: "res_num1")[0].value;
-    num2 = document.getElementsByName(elementName: "res_num2")[0].value;
+    num1 = document.getElementsByName(elementName:"res_num1")[0].value;
+    num2 = document.getElementsByName(elementName:"res_num2")[0].value;
     document.getElementsByName(elementName:"res_total")[0].innerHTML = Number(num1) - Number(num2);
 }
 
-function calcularMultiplicacion(){
+function calcularMultiplicacion() {
     var num1, num2;
-    num1 = document.getElementsByName(elementName: "mul_num1")[0].value;
-    num2 = document.getElementsByName(elementName: "mul_num2")[0].value;
+    num1 = document.getElementsByName(elementName:"mul_num1")[0].value;
+    num2 = document.getElementsByName(elementName:"mul_num2")[0].value;
     document.getElementsByName(elementName:"mul_total")[0].innerHTML = Number(num1) * Number(num2);
 }
 
-function calcularDivision(){
+function calcularDivision() {
     var num1, num2;
-    num1 = document.getElementsByName(elementName: "div_num1")[0].value;
-    num2 = document.getElementsByName(elementName: "div_num2")[0].value;
+    num1 = document.getElementsByName(elementName:"div_num1")[0].value;
+    num2 = document.getElementsByName(elementName:"div_num2")[0].value;
     document.getElementsByName(elementName:"div_total")[0].innerHTML = Number(num1) / Number(num2);
 }
 
-function cargarWeb(){
+function cargarWeb() {
     var cant, unidad, urlComp;
-    cant = document.getElementById(elementId: "distancia").value;
+    cant = document.getElementById(elementId:"distancia").value;
     unidad = document.getElementsByName(elementName:"")[0].value;
     urlComp = "segundaWeb.html#" + cant + "#" + unidad;
     window.open(urlComp);
 }
 
-function cargarResultado(){
+function cargarResultado() {
     var urlComp, can, un;
-    urlComp = window.location.href.split(separator: "/")[5];
-    can = urlComp.split(separator: "#")[1];
-    un = urlComp.split(separator: "#")[2];
-    document.getElementById(elementId: "dist").value = can + " " + un;
+    urlComp = window.location.href.split(separator:"/")[5];
+    can = urlComp.split(separator:"#")[1];
+    un = urlComp.split(separator:"#")[2];
+    document.getElementById(elementId:"dist").value = can + " " + un;
+}
+
+function guardarLocalStorage() {
+    let distancia, unidad;
+    distancia = document.getElementById(elementId:'distancia').value;
+    unidad = document.getElementsByName(elementName:'unidades')[0].value;
+    localStorage.setItem("distanciaLS", distancia);
+    localStorage.setItem("unidadesLS", unidad);
+    window.open(url:'2_web.html');
+}
+
+function cargarLocalStorage() {
+    let cant, un;
+    cant = localStorage.getItem(key:"distanciaLS");
+    un = localStorage.getItem(key:"unidadesLS");
+    document.getElementById(elementId:"dist").value = cant + " " + un;
 }
