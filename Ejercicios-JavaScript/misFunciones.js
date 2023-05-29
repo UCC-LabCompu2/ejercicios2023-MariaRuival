2d5b7e75d50c6267b78344bf4c6ce6086aabb129
MariaRuival <2207446@ucc.edu.ar>
2023-05-29 16:09:01 -0300

    Se agregó el ejercicio de Mini Paint

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -138,3 +138,27 @@ function dibujarCirCuad(){
     ctx.fillStyle = "#d90b0b";
     ctx.fill();
 }
+var bandera;
+function dibujar(event){
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    var posX = event.clientX;
+    var posY = event.clientY;
+    console.log(posX,posY);
+
+    canvas.onmousedown = function(){bandera = true};
+    canvas.onmouseup = function(){bandera = false};
+
+    if(bandera) {
+        ctx.fillRect(posX, posY, 5, 5);
+        ctx.fill;
+    }
+}
+
+function limpiarCanvas(){
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+}
